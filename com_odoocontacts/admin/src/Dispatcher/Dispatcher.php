507c2c6012ae12c4570d0cdf5952d325261fce63
plgmgua <plgmgua@gmail.com>
2026-01-13 09:00:30 -0600

    feat: Add admin Dispatcher class for Joomla 5 compatibility

---
+++ com_odoocontacts/admin/src/Dispatcher/Dispatcher.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @package     Grimpsa.Administrator
+ * @subpackage  com_odoocontacts
+ *
+ * @copyright   Copyright (C) 2025 Grimpsa. All rights reserved.
+ * @license     GNU General Public License version 2 or later
+ */
+
+namespace Grimpsa\Component\OdooContacts\Administrator\Dispatcher;
+
+defined('_JEXEC') or die;
+
+use Joomla\CMS\Dispatcher\ComponentDispatcher;
+
+/**
+ * ComponentDispatcher class for com_odoocontacts
+ */
+class Dispatcher extends ComponentDispatcher
+{
+    /**
+     * The extension namespace
+     *
+     * @var    string
+     */
+    protected $namespace = 'Grimpsa\\Component\\OdooContacts';
+}
+
